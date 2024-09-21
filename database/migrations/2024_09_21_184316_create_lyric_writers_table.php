8d2a6c9cb9ea37eb9f1ad49b2b39fcfe8a91e3ba
Masaki1152 <takanenoharusaku@gmail.com>
2024-09-21 18:45:55 +0900

    feature:作品関連者のテーブルの追加

---
+++ database/migrations/2024_09_21_184316_create_lyric_writers_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('lyric_writers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('wiki_link');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('lyric_writers');
+    }
+};
