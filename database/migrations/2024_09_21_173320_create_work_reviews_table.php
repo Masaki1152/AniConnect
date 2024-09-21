25342380ede2e9316fc2525e9e1c35702142bb33
Masaki1152 <takanenoharusaku@gmail.com>
2024-09-21 17:48:31 +0900

    feature:作品全体に関するテーブルの追加

---
+++ database/migrations/2024_09_21_173320_create_work_reviews_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('work_reviews', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('work_id');
+            $table->unsignedInteger('user_id');
+            $table->string('post_title');
+            $table->string('body');
+            $table->string('image1')->nullable();
+            $table->string('image2')->nullable();
+            $table->string('image3')->nullable();
+            $table->string('image4')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('work_reviews');
+    }
+};
