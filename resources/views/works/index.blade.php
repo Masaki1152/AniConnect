bd96b746c38c0709735cfdab6a723af02cfaa3ff
Masaki1152 <takanenoharusaku@gmail.com>
2024-11-03 00:28:32 +0900

    feat:一覧表示しなおすキャンセルボタンの追加

--- resources/views/works/index.blade.php
+++ resources/views/works/index.blade.php
@@ -13,22 +13,25 @@
     <!-- 検索機能 -->
     <div class=serch>
         <form action="{{ route('works.index') }}" method="GET">
-            <input type="text" name="search" value="{{ request('serch') }}" placeholder="キーワードを検索" aria-label="検索...">
+            <input type="text" name="search" value="{{ request('search') }}" placeholder="キーワードを検索" aria-label="検索...">
             <input type="submit" value="検索">
         </form>
+        <div class="cancel">
+            <a href="{{ route('works.index') }}">キャンセル</a>
+        </div>
     </div>
     <div class='works'>
         @if($works->isEmpty())
         <h2 class='no_result'>結果がありません。</h2>
         @else
-            @foreach ($works as $work)
-            <div class='work'>
-                <h2 class='name'>
-                    <a href="/works/{{ $work->id }}">{{ $work->name }}</a>
-                </h2>
-                <p class='term'>{{ $work->term }}</p>
-            </div>
-            @endforeach
+        @foreach ($works as $work)
+        <div class='work'>
+            <h2 class='name'>
+                <a href="/works/{{ $work->id }}">{{ $work->name }}</a>
+            </h2>
+            <p class='term'>{{ $work->term }}</p>
+        </div>
+        @endforeach
         @endif
     </div>
     <div class='paginate'>
