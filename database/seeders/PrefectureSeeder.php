e018135b07696116496553e82997b731ba7a7c93
Masaki1152 <takanenoharusaku@gmail.com>
2024-11-06 20:47:43 +0900

    feat:県名テーブルの修正

--- database/seeders/PrefectureSeeder.php
+++ database/seeders/PrefectureSeeder.php
@@ -15,22 +15,102 @@ class PrefectureSeeder extends Seeder
     public function run(): void
     {
         DB::table('prefectures')->insert([
-            'name' => '東京都',
+            'name' => '北海道',
             'created_at' => new DateTime(),
             'updated_at' => new DateTime(),
         ]);
         DB::table('prefectures')->insert([
-            'name' => '大阪府',
+            'name' => '青森県',
             'created_at' => new DateTime(),
             'updated_at' => new DateTime(),
         ]);
         DB::table('prefectures')->insert([
-            'name' => '京都府',
+            'name' => '岩手県',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('prefectures')->insert([
+            'name' => '宮城県',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('prefectures')->insert([
+            'name' => '秋田県',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('prefectures')->insert([
+            'name' => '山形県',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('prefectures')->insert([
+            'name' => '福島県',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('prefectures')->insert([
+            'name' => '茨城県',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('prefectures')->insert([
+            'name' => '栃木県',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('prefectures')->insert([
+            'name' => '群馬県',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('prefectures')->insert([
+            'name' => '埼玉県',
             'created_at' => new DateTime(),
             'updated_at' => new DateTime(),
         ]);
         DB::table('prefectures')->insert([
             'name' => '千葉県',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('prefectures')->insert([
+            'name' => '東京都',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('prefectures')->insert([
+            'name' => '神奈川県',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('prefectures')->insert([
+            'name' => '新潟県',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('prefectures')->insert([
+            'name' => '富山県',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('prefectures')->insert([
+            'name' => '石川県',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('prefectures')->insert([
+            'name' => '福井県',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('prefectures')->insert([
+            'name' => '山梨県',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('prefectures')->insert([
+            'name' => '長野県',
             'created_at' => new DateTime(),
             'updated_at' => new DateTime(),
         ]);
@@ -41,6 +121,131 @@ class PrefectureSeeder extends Seeder
         ]);
         DB::table('prefectures')->insert([
             'name' => '静岡県',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('prefectures')->insert([
+            'name' => '愛知県',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('prefectures')->insert([
+            'name' => '三重県',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('prefectures')->insert([
+            'name' => '滋賀県',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('prefectures')->insert([
+            'name' => '京都府',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('prefectures')->insert([
+            'name' => '大阪府',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('prefectures')->insert([
+            'name' => '兵庫県',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('prefectures')->insert([
+            'name' => '奈良県',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('prefectures')->insert([
+            'name' => '和歌山県',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('prefectures')->insert([
+            'name' => '鳥取県',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('prefectures')->insert([
+            'name' => '島根県',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('prefectures')->insert([
+            'name' => '岡山県',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('prefectures')->insert([
+            'name' => '広島県',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('prefectures')->insert([
+            'name' => '山口県',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('prefectures')->insert([
+            'name' => '徳島県',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('prefectures')->insert([
+            'name' => '香川県',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('prefectures')->insert([
+            'name' => '愛媛県',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('prefectures')->insert([
+            'name' => '高知県',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('prefectures')->insert([
+            'name' => '福岡県',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('prefectures')->insert([
+            'name' => '佐賀県',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('prefectures')->insert([
+            'name' => '長崎県',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('prefectures')->insert([
+            'name' => '熊本県',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('prefectures')->insert([
+            'name' => '大分県',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('prefectures')->insert([
+            'name' => '宮崎県',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('prefectures')->insert([
+            'name' => '鹿児島県',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('prefectures')->insert([
+            'name' => '沖縄県',
             'created_at' => new DateTime(),
             'updated_at' => new DateTime(),
         ]);
