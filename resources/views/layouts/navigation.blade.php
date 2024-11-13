ae014d6305b09b754fc50936b39726df9d7f32c8
Masaki1152 <takanenoharusaku@gmail.com>
2024-11-13 18:06:51 +0900

    refactor:navigation.blade.phpの登場人物一覧の記述を修正

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -18,7 +18,7 @@
                     <x-nav-link :href="route('works.index')" :active="request()->routeIs('works.index')">
                         {{ __('作品一覧') }}
                     </x-nav-link>
-                    <x-nav-link :href="route('characters.index')" :active="request()->routeIs('works.index')">
+                    <x-nav-link :href="route('characters.index')" :active="request()->routeIs('characters.index')">
                         {{ __('登場人物一覧') }}
                     </x-nav-link>
                     <x-nav-link :href="route('music.index')" :active="request()->routeIs('music.index')">
