c699b409ef9445ce28e4016f2c6a189eabc16b5e
Masaki1152 <takanenoharusaku@gmail.com>
2024-11-21 22:53:06 +0900

    feat:聖地感想投稿に新プレビュー機能の追加

--- resources/views/anime_pilgrimage_posts/edit.blade.php
+++ resources/views/anime_pilgrimage_posts/edit.blade.php
@@ -1,23 +1,28 @@
 <x-app-layout>
     <h1 class="title">{{ $pilgrimage_post->animePilgrimage->name }}への投稿編集画面</h1>
     <div class="content">
-        <form action="{{ route('pilgrimage_posts.update', ['pilgrimage_id' => $pilgrimage_post->anime_pilgrimage_id, 'pilgrimage_post_id' => $pilgrimage_post->id]) }}" method="POST" enctype="multipart/form-data">
+        <form
+            action="{{ route('pilgrimage_posts.update', ['pilgrimage_id' => $pilgrimage_post->anime_pilgrimage_id, 'pilgrimage_post_id' => $pilgrimage_post->id]) }}"
+            method="POST" enctype="multipart/form-data">
             @csrf
             @method('PUT')
             <div class="user_id">
                 <input type="hidden" name="pilgrimage_post[user_id]" value="{{ $pilgrimage_post->user_id }}">
             </div>
             <div class="anime_pilgrimage_id">
-                <input type="hidden" name="pilgrimage_post[anime_pilgrimage_id]" value="{{ $pilgrimage_post->anime_pilgrimage_id }}">
+                <input type="hidden" name="pilgrimage_post[anime_pilgrimage_id]"
+                    value="{{ $pilgrimage_post->anime_pilgrimage_id }}">
             </div>
             <div class="title">
                 <h2>タイトル</h2>
-                <input type="text" name="pilgrimage_post[title]" placeholder="タイトル" value="{{ $pilgrimage_post->title }}" />
+                <input type="text" name="pilgrimage_post[title]" placeholder="タイトル"
+                    value="{{ $pilgrimage_post->title }}" />
                 <p class="title__error" style="color:red">{{ $errors->first('pilgrimage_post.title') }}</p>
             </div>
             <div class="scene">
                 <h2>シーン</h2>
-                <input type="text" name="pilgrimage_post[scene]" placeholder="シーン" value="{{ $pilgrimage_post->scene }}" />
+                <input type="text" name="pilgrimage_post[scene]" placeholder="シーン"
+                    value="{{ $pilgrimage_post->scene }}" />
                 <p class="title__error" style="color:red">{{ $errors->first('pilgrimage_post.scene') }}</p>
             </div>
             <div class="body">
@@ -27,7 +32,28 @@
             </div>
             <div class="image">
                 <h2>画像（4枚まで）</h2>
-                <input id="inputElm" type="file" name="images[]" multiple onchange="loadImage(this);">
+                @php
+                    // 既にファイルが選択されている場合はそれらを表示する
+                    $existingImages = [];
+                    $numbers = [1, 2, 3, 4];
+                    foreach ($numbers as $number) {
+                        $image = 'image' . $number;
+                        if ($pilgrimage_post->$image) {
+                            array_push($existingImages, $pilgrimage_post->$image);
+                        }
+                    }
+                    $existingImages = json_encode($existingImages);
+                @endphp
+                <div id="existing_image_paths" data-php-variable="{{ $existingImages }}"></div>
+                <label>
+                    <input id="inputElm" type="file" style="display:none" name="images[]" multiple
+                        onchange="loadImage(this);">画像の追加
+                    <div id="count"></div>
+                </label>
+                <!-- 削除された既存画像のリスト -->
+                <input type="hidden" name="removedImages[]" id="removedImages" value="">
+                <!-- 削除されず残った既存画像のリスト -->
+                <input type="hidden" name="remainedImages[]" id="remainedImages" value="">
                 <p class="image__error" style="color:red">{{ $errors->first('images') }}</p>
             </div>
             <!-- プレビュー画像の表示 -->
@@ -36,40 +62,8 @@
         </form>
     </div>
     <div class="footer">
-        <a href="{{ route('pilgrimage_posts.show', ['pilgrimage_id' => $pilgrimage_post->anime_pilgrimage_id, 'pilgrimage_post_id' => $pilgrimage_post->id]) }}">保存しないで戻る</a>
+        <a
+            href="{{ route('pilgrimage_posts.show', ['pilgrimage_id' => $pilgrimage_post->anime_pilgrimage_id, 'pilgrimage_post_id' => $pilgrimage_post->id]) }}">保存しないで戻る</a>
     </div>
-    <script>
-        let key = 0;
-
-        function loadImage(obj) {
-            // 以前に選択したファイルは保持されないため削除
-            document.querySelectorAll('figure').forEach(function(figure) {
-                figure.remove();
-                key = 0;
-            });
-            // 選択されたファイルの枚数分だけ画像を追加
-            for (i = 0; i < obj.files.length; i++) {
-                var fileReader = new FileReader();
-                fileReader.onload = (function(e) {
-                    var field = document.getElementById("preview");
-                    var figure = document.createElement("figure");
-                    var rmBtn = document.createElement("input");
-                    var img = new Image();
-                    img.src = e.target.result;
-                    rmBtn.type = "button";
-                    rmBtn.name = key;
-                    rmBtn.value = "削除";
-                    rmBtn.onclick = (function() {
-                        var element = document.getElementById("img-" + String(rmBtn.name)).remove();
-                    });
-                    figure.setAttribute("id", "img-" + key);
-                    figure.appendChild(img);
-                    figure.appendChild(rmBtn)
-                    field.appendChild(figure);
-                    key++;
-                });
-                fileReader.readAsDataURL(obj.files[i]);
-            }
-        }
-    </script>
+    <script src="{{ asset('/js/edit_preview.js') }}"></script>
 </x-app-layout>
