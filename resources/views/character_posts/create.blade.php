ed71c7b0f1988683962fb1b4743ad67bd92f4ca4
Masaki1152 <takanenoharusaku@gmail.com>
2024-11-21 22:39:49 +0900

    feat:登場人物感想に新プレビュー機能の反映

--- resources/views/character_posts/create.blade.php
+++ resources/views/character_posts/create.blade.php
@@ -1,6 +1,7 @@
 <x-app-layout>
-    <h1>「{{$character_post->character->name}}」への新規感想投稿</h1>
-    <form action="{{ route('character_posts.store', ['character_id' => $character_post->character_id]) }}" method="POST" enctype="multipart/form-data">
+    <h1>「{{ $character_post->character->name }}」への新規感想投稿</h1>
+    <form action="{{ route('character_posts.store', ['character_id' => $character_post->character_id]) }}" method="POST"
+        enctype="multipart/form-data">
         @csrf
         <div class="work_id">
             <input type="hidden" name="character_post[work_id]" value="{{ $character_post->work_id }}">
@@ -10,33 +11,35 @@
         </div>
         <div class="title">
             <h2>タイトル</h2>
-            <input type="text" name="character_post[post_title]" placeholder="タイトル" value="{{ old('character_post.post_title') }}" />
+            <input type="text" name="character_post[post_title]" placeholder="タイトル"
+                value="{{ old('character_post.post_title') }}" />
             <p class="title__error" style="color:red">{{ $errors->first('character_post.post_title') }}</p>
         </div>
         <div class="star_num">
             <h2>星の数</h2>
             <select name="character_post[star_num]">
                 @php
-                $numbers = array(1 => '★', 2 => '★★', 3 => '★★★', 4 => '★★★★', 5 => '★★★★★');
+                    $numbers = [1 => '★', 2 => '★★', 3 => '★★★', 4 => '★★★★', 5 => '★★★★★'];
                 @endphp
-                @foreach($numbers as $num => $star)
-                <option value="{{ $num }}" @if(old('character_post.star_num')==$num) selected @endif>
-                    {{$star}}
-                </option>
+                @foreach ($numbers as $num => $star)
+                    <option value="{{ $num }}" @if (old('character_post.star_num') == $num) selected @endif>
+                        {{ $star }}
+                    </option>
                 @endforeach
             </select>
         </div>
         <div class="category">
             <h2>カテゴリー（3個まで）</h2>
             <select name="character_post[categories_array][]" multiple>
-                @foreach($categories as $category)
-                <option value="{{ $category->id }}" @if(in_array($category->id, old('character_post.categories_array', []))) selected @endif>
-                    {{$category->name}}
-                </option>
+                @foreach ($categories as $category)
+                    <option value="{{ $category->id }}" @if (in_array($category->id, old('character_post.categories_array', []))) selected @endif>
+                        {{ $category->name }}
+                    </option>
                 @endforeach
             </select>
             @if ($errors->has('character_post.categories_array'))
-            <p class="category__error" style="color:red">{{ $errors->first('character_post.categories_array') }}</p>
+                <p class="category__error" style="color:red">{{ $errors->first('character_post.categories_array') }}
+                </p>
             @endif
         </div>
         <div class="body">
@@ -46,7 +49,11 @@
         </div>
         <div class="image">
             <h2>画像（4枚まで）</h2>
-            <input id="inputElm" type="file" name="images[]" multiple onchange="loadImage(this);">
+            <label>
+                <input id="inputElm" type="file" style="display:none" name="images[]" multiple
+                    onchange="loadImage(this);">画像の追加
+                <div id="count">現在、0枚の画像を選択しています。</div>
+            </label>
             <p class="image__error" style="color:red">{{ $errors->first('images') }}</p>
         </div>
         <!-- プレビュー画像の表示 -->
@@ -56,39 +63,5 @@
     <div class="footer">
         <a href="{{ route('character_posts.index', ['character_id' => $character_post->character_id]) }}">戻る</a>
     </div>
-    <script>
-        let key = 0;
-
-        function loadImage(obj) {
-            // 以前に選択したファイルは保持されないため削除
-            document.querySelectorAll('figure').forEach(function(figure) {
-                figure.remove();
-                key = 0;
-            });
-            // 選択されたファイルの枚数分だけ画像を追加
-            for (i = 0; i < obj.files.length; i++) {
-                var fileReader = new FileReader();
-                fileReader.onload = (function(e) {
-                    var field = document.getElementById("preview");
-                    var figure = document.createElement("figure");
-                    var rmBtn = document.createElement("input");
-                    var img = new Image();
-                    img.src = e.target.result;
-                    rmBtn.type = "button";
-                    rmBtn.name = key;
-                    rmBtn.value = "削除";
-                    // 削除ボタン押下で画像プレビューの削除
-                    rmBtn.onclick = (function() {
-                        var element = document.getElementById("img-" + String(rmBtn.name)).remove();
-                    });
-                    figure.setAttribute("id", "img-" + key);
-                    figure.appendChild(img);
-                    figure.appendChild(rmBtn)
-                    field.appendChild(figure);
-                    key++;
-                });
-                fileReader.readAsDataURL(obj.files[i]);
-            }
-        }
-    </script>
+    <script src="{{ asset('/js/create_preview.js') }}"></script>
 </x-app-layout>
--- resources/views/character_posts/edit.blade.php
+++ resources/views/character_posts/edit.blade.php
@@ -1,7 +1,9 @@
 <x-app-layout>
     <h1 class="title">{{ $character_post->character->name }}への投稿編集画面</h1>
     <div class="content">
-        <form action="{{ route('character_posts.update', ['character_id' => $character_post->character_id, 'character_post_id' => $character_post->id]) }}" method="POST" enctype="multipart/form-data">
+        <form
+            action="{{ route('character_posts.update', ['character_id' => $character_post->character_id, 'character_post_id' => $character_post->id]) }}"
+            method="POST" enctype="multipart/form-data">
             @csrf
             @method('PUT')
             <div class="work_id">
@@ -15,40 +17,42 @@
             </div>
             <div class="title">
                 <h2>タイトル</h2>
-                <input type="text" name="character_post[post_title]" placeholder="タイトル" value="{{ $character_post->post_title }}" />
+                <input type="text" name="character_post[post_title]" placeholder="タイトル"
+                    value="{{ $character_post->post_title }}" />
                 <p class="title__error" style="color:red">{{ $errors->first('character_post.post_title') }}</p>
             </div>
             <div class="star_num">
                 <h2>星の数</h2>
                 <select name="character_post[star_num]">
                     @php
-                    $numbers = array(1 => '★', 2 => '★★', 3 => '★★★', 4 => '★★★★', 5 => '★★★★★');
+                        $numbers = [1 => '★', 2 => '★★', 3 => '★★★', 4 => '★★★★', 5 => '★★★★★'];
                     @endphp
-                    @foreach($numbers as $num => $star)
-                    <option value="{{ $num }}" @if($character_post->star_num == $num) selected @endif>
-                        {{$star}}
-                    </option>
+                    @foreach ($numbers as $num => $star)
+                        <option value="{{ $num }}" @if ($character_post->star_num == $num) selected @endif>
+                            {{ $star }}
+                        </option>
                     @endforeach
                 </select>
             </div>
             <div class="category">
                 <h2>カテゴリー（3個まで）</h2>
                 @php
-                // old() が存在すればそれを使用し、なければ過去の保存値を使用
-                $existingCategories = $character_post->categories->pluck('id')->toArray();
-                $selectedCategories = old('character_post.categories_array', $existingCategories);
+                    // old() が存在すればそれを使用し、なければ過去の保存値を使用
+                    $existingCategories = $character_post->categories->pluck('id')->toArray();
+                    $selectedCategories = old('character_post.categories_array', $existingCategories);
                 @endphp
                 <select name="character_post[categories_array][]" multiple>
-                    @foreach($categories as $category)
-                    <option value="{{ $category->id }}"
-                        {{ in_array($category->id, $selectedCategories) ? 'selected' : '' }}>
-                        {{ $category->name }}
-                    </option>
+                    @foreach ($categories as $category)
+                        <option value="{{ $category->id }}"
+                            {{ in_array($category->id, $selectedCategories) ? 'selected' : '' }}>
+                            {{ $category->name }}
+                        </option>
                     @endforeach
                 </select>
 
                 @if ($errors->has('character_post.categories_array'))
-                <p class="category__error" style="color:red">{{ $errors->first('character_post.categories_array') }}</p>
+                    <p class="category__error" style="color:red">
+                        {{ $errors->first('character_post.categories_array') }}</p>
                 @endif
             </div>
             <div class="body">
@@ -58,7 +62,28 @@
             </div>
             <div class="image">
                 <h2>画像（4枚まで）</h2>
-                <input id="inputElm" type="file" name="images[]" multiple onchange="loadImage(this);">
+                @php
+                    // 既にファイルが選択されている場合はそれらを表示する
+                    $existingImages = [];
+                    $numbers = [1, 2, 3, 4];
+                    foreach ($numbers as $number) {
+                        $image = 'image' . $number;
+                        if ($character_post->$image) {
+                            array_push($existingImages, $character_post->$image);
+                        }
+                    }
+                    $existingImages = json_encode($existingImages);
+                @endphp
+                <div id="existing_image_paths" data-php-variable="{{ $existingImages }}"></div>
+                <label>
+                    <input id="inputElm" type="file" style="display:none" name="images[]" multiple
+                        onchange="loadImage(this);">画像の追加
+                    <div id="count"></div>
+                </label>
+                <!-- 削除された既存画像のリスト -->
+                <input type="hidden" name="removedImages[]" id="removedImages" value="">
+                <!-- 削除されず残った既存画像のリスト -->
+                <input type="hidden" name="remainedImages[]" id="remainedImages" value="">
                 <p class="image__error" style="color:red">{{ $errors->first('images') }}</p>
             </div>
             <!-- プレビュー画像の表示 -->
@@ -67,40 +92,8 @@
         </form>
     </div>
     <div class="footer">
-        <a href="{{ route('character_posts.show', ['character_id' => $character_post->character_id, 'character_post_id' => $character_post->id]) }}">保存しないで戻る</a>
+        <a
+            href="{{ route('character_posts.show', ['character_id' => $character_post->character_id, 'character_post_id' => $character_post->id]) }}">保存しないで戻る</a>
     </div>
-    <script>
-        let key = 0;
-
-        function loadImage(obj) {
-            // 以前に選択したファイルは保持されないため削除
-            document.querySelectorAll('figure').forEach(function(figure) {
-                figure.remove();
-                key = 0;
-            });
-            // 選択されたファイルの枚数分だけ画像を追加
-            for (i = 0; i < obj.files.length; i++) {
-                var fileReader = new FileReader();
-                fileReader.onload = (function(e) {
-                    var field = document.getElementById("preview");
-                    var figure = document.createElement("figure");
-                    var rmBtn = document.createElement("input");
-                    var img = new Image();
-                    img.src = e.target.result;
-                    rmBtn.type = "button";
-                    rmBtn.name = key;
-                    rmBtn.value = "削除";
-                    rmBtn.onclick = (function() {
-                        var element = document.getElementById("img-" + String(rmBtn.name)).remove();
-                    });
-                    figure.setAttribute("id", "img-" + key);
-                    figure.appendChild(img);
-                    figure.appendChild(rmBtn)
-                    field.appendChild(figure);
-                    key++;
-                });
-                fileReader.readAsDataURL(obj.files[i]);
-            }
-        }
-    </script>
+    <script src="{{ asset('/js/edit_preview.js') }}"></script>
 </x-app-layout>
