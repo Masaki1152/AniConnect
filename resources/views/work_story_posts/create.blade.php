14c7dc229a07db919c557ed0a392dc274e84e85d
Masaki1152 <takanenoharusaku@gmail.com>
2024-11-21 22:12:27 +0900

    feat:あらすじ感想にもプレビュー機能の修正反映

--- resources/views/work_story_posts/create.blade.php
+++ resources/views/work_story_posts/create.blade.php
@@ -1,6 +1,8 @@
 <x-app-layout>
     <h1>「{{ $work_story_post->workStory->sub_title }}」への新規感想投稿</h1>
-    <form action="{{ route('work_story_posts.store', ['work_id' => $work_story_post->work_id, 'work_story_id' => $work_story_post->sub_title_id, 'work_story_post_id' => $work_story_post->id]) }}" method="POST" enctype="multipart/form-data">
+    <form
+        action="{{ route('work_story_posts.store', ['work_id' => $work_story_post->work_id, 'work_story_id' => $work_story_post->sub_title_id, 'work_story_post_id' => $work_story_post->id]) }}"
+        method="POST" enctype="multipart/form-data">
         @csrf
         <div class="work_id">
             <input type="hidden" name="work_story_post[work_id]" value="{{ $work_story_post->work_id }}">
@@ -10,7 +12,8 @@
         </div>
         <div class="title">
             <h2>タイトル</h2>
-            <input type="text" name="work_story_post[post_title]" placeholder="タイトル" value="{{ old('work_story_post.post_title') }}" />
+            <input type="text" name="work_story_post[post_title]" placeholder="タイトル"
+                value="{{ old('work_story_post.post_title') }}" />
             <p class="title__error" style="color:red">{{ $errors->first('work_story_post.post_title') }}</p>
         </div>
         <div class="body">
@@ -20,7 +23,11 @@
         </div>
         <div class="image">
             <h2>画像（4枚まで）</h2>
-            <input id="inputElm" type="file" name="images[]" multiple onchange="loadImage(this);">
+            <label>
+                <input id="inputElm" type="file" style="display:none" name="images[]" multiple
+                    onchange="loadImage(this);">画像の追加
+                <div id="count">現在、0枚の画像を選択しています。</div>
+            </label>
             <p class="image__error" style="color:red">{{ $errors->first('images') }}</p>
         </div>
         <!-- プレビュー画像の表示 -->
@@ -28,42 +35,9 @@
         <button type="submit">投稿する</button>
     </form>
     <div class="footer">
-        <a href="{{ route('work_story_posts.index', ['work_id' => $work_story_post->work_id, 'work_story_id' => $work_story_post->sub_title_id]) }}">戻る</a>
+        <a
+            href="{{ route('work_story_posts.index', ['work_id' => $work_story_post->work_id, 'work_story_id' => $work_story_post->sub_title_id]) }}">戻る</a>
     </div>
-    <script>
-        let key = 0;
-
-        function loadImage(obj) {
-            // 以前に選択したファイルは保持されないため削除
-            document.querySelectorAll('figure').forEach(function(figure) {
-                figure.remove();
-                key = 0;
-            });
-            // 選択されたファイルの枚数分だけ画像を追加
-            for (i = 0; i < obj.files.length; i++) {
-                var fileReader = new FileReader();
-                fileReader.onload = (function(e) {
-                    var field = document.getElementById("preview");
-                    var figure = document.createElement("figure");
-                    var rmBtn = document.createElement("input");
-                    var img = new Image();
-                    img.src = e.target.result;
-                    rmBtn.type = "button";
-                    rmBtn.name = key;
-                    rmBtn.value = "削除";
-                    // 削除ボタン押下で画像プレビューの削除
-                    rmBtn.onclick = (function() {
-                        var element = document.getElementById("img-" + String(rmBtn.name)).remove();
-                    });
-                    figure.setAttribute("id", "img-" + key);
-                    figure.appendChild(img);
-                    figure.appendChild(rmBtn)
-                    field.appendChild(figure);
-                    key++;
-                });
-                fileReader.readAsDataURL(obj.files[i]);
-            }
-        }
-    </script>
+    <script src="{{ asset('/js/create_preview.js') }}"></script>
 
 </x-app-layout>
