89752e438550b83361e267c9f3332e388b091da0
Masaki1152 <takanenoharusaku@gmail.com>
2024-12-06 19:31:12 +0900

    feat:いいね機能のjs記述を外部ファイルに移動

--- resources/views/work_story_posts/index.blade.php
+++ resources/views/work_story_posts/index.blade.php
@@ -82,6 +82,8 @@
         <div class='paginate'>
             {{ $work_story_posts->appends(request()->query())->links() }}
         </div>
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <script src="{{ asset('/js/like_posts/like_work_story_post.js') }}"></script>
         <script>
             // DOMツリー読み取り完了後にイベント発火
             document.addEventListener('DOMContentLoaded', function() {
@@ -102,56 +104,6 @@
                     document.getElementById(`form_${postId}`).submit();
                 }
             }
-
-            // いいね処理を非同期で行う
-            document.addEventListener('DOMContentLoaded', function() {
-                const likeClasses = document.querySelectorAll('.like');
-                const likeMessage = document.getElementById('like-message');
-                likeClasses.forEach(element => {
-                    // いいねボタンのクラスの取得
-                    let button = element.querySelector('#like_button');
-                    // いいねしたユーザー数のクラス取得とpタグの取得
-                    let likeClass = element.querySelector('.like_user');
-                    let users = likeClass.querySelector('#like_count');
-
-                    //いいねボタンクリックによる非同期処理
-                    button.addEventListener('click', async function() {
-                        const workId = button.getAttribute('data-work-id');
-                        const workStoryId = button.getAttribute('data-work_story-id');
-                        const postId = button.getAttribute('data-post-id');
-                        try {
-                            const response = await fetch(
-                                `/works/${workId}/stories/${workStoryId}/posts/${postId}/like`, {
-                                    method: 'POST',
-                                    headers: {
-                                        'Content-Type': 'application/json',
-                                        'X-CSRF-TOKEN': '{{ csrf_token() }}'
-                                    },
-                                });
-                            const data = await response.json();
-                            if (data.status === 'liked') {
-                                button.innerText = 'いいね取り消し';
-                                users.innerText = data.like_user;
-                            } else if (data.status === 'unliked') {
-                                button.innerText = 'いいね';
-                                users.innerText = data.like_user;
-                            }
-                            // メッセージを表示
-                            likeMessage.textContent = data.message;
-                            likeMessage.classList.remove('hidden');
-                            likeMessage.classList.add('block');
-
-                            // 3秒後にメッセージを非表示
-                            setTimeout(() => {
-                                likeMessage.classList.add('hidden');
-                                likeMessage.classList.remove('block');
-                            }, 3000);
-                        } catch (error) {
-                            console.error('Error:', error);
-                        }
-                    });
-                });
-            });
         </script>
     @endif
 </x-app-layout>
