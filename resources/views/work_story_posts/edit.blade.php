cdfec28e9700e2112ce27267570971d708aa268d
Masaki1152 <takanenoharusaku@gmail.com>
2025-01-08 23:14:36 +0900

    feat:あらすじ感想の編集画面のデザイン修正

--- resources/views/work_story_posts/edit.blade.php
+++ resources/views/work_story_posts/edit.blade.php
@@ -1,86 +1,177 @@
 <x-app-layout>
-    <h1 class="title">「{{ $work_story_post->workStory->sub_title }}」への投稿編集画面</h1>
-    <div class="content">
-        <form
-            action="{{ route('work_story_posts.update', ['work_id' => $work_story_post->work_id, 'work_story_id' => $work_story_post->sub_title_id, 'work_story_post_id' => $work_story_post->id]) }}"
-            method="POST" enctype="multipart/form-data">
-            @csrf
-            @method('PUT')
-            <div class="work_id">
-                <input type="hidden" name="work_story_post[work_id]" value="{{ $work_story_post->work_id }}">
-            </div>
-            <div class="user_id">
-                <input type="hidden" name="work_story_post[user_id]" value="{{ $work_story_post->user_id }}">
-            </div>
-            <div class="sub_title_id">
-                <input type="hidden" name="work_story_post[sub_title_id]" value="{{ $work_story_post->sub_title_id }}">
-            </div>
-            <div class="title">
-                <h2>タイトル</h2>
-                <input type="text" name="work_story_post[post_title]" placeholder="タイトル"
-                    value="{{ $work_story_post->post_title }}" />
-                <p class="title__error" style="color:red">{{ $errors->first('work_story_post.post_title') }}</p>
-            </div>
-            <div class="category">
-                <h2>カテゴリー（3個まで）</h2>
-                @php
-                    // old() が存在すればそれを使用し、なければ過去の保存値を使用
-                    $existingCategories = $work_story_post->categories->pluck('id')->toArray();
-                    $selectedCategories = old('work_story_post.categories_array', $existingCategories);
-                @endphp
-                <select name="work_story_post[categories_array][]" multiple>
-                    @foreach ($categories as $category)
-                        <option value="{{ $category->id }}"
-                            {{ in_array($category->id, $selectedCategories) ? 'selected' : '' }}>
-                            {{ $category->name }}
-                        </option>
-                    @endforeach
-                </select>
-
-                @if ($errors->has('work_story_post.categories_array'))
-                    <p class="category__error" style="color:red">
-                        {{ $errors->first('work_story_post.categories_array') }}</p>
-                @endif
-            </div>
-            <div class="body">
-                <h2>内容</h2>
-                <textarea name="work_story_post[body]" placeholder="内容を記入してください。">{{ $work_story_post->body }}</textarea>
-                <p class="body__error" style="color:red">{{ $errors->first('work_story_post.body') }}</p>
-            </div>
-            <div class="image">
-                <h2>画像（4枚まで）</h2>
-                @php
-                    // 既にファイルが選択されている場合はそれらを表示する
-                    $existingImages = [];
-                    $numbers = [1, 2, 3, 4];
-                    foreach ($numbers as $number) {
-                        $image = 'image' . $number;
-                        if ($work_story_post->$image) {
-                            array_push($existingImages, $work_story_post->$image);
-                        }
-                    }
-                    $existingImages = json_encode($existingImages);
-                @endphp
-                <div id="existing_image_paths" data-php-variable="{{ $existingImages }}"></div>
-                <label>
-                    <input id="inputElm" type="file" style="display:none" name="images[]" multiple
-                        onchange="loadImage(this);">画像の追加
-                    <div id="count"></div>
-                </label>
-                <!-- 削除された既存画像のリスト -->
-                <input type="hidden" name="removedImages[]" id="removedImages" value="">
-                <!-- 削除されず残った既存画像のリスト -->
-                <input type="hidden" name="remainedImages[]" id="remainedImages" value="">
-                <p class="image__error" style="color:red">{{ $errors->first('images') }}</p>
-            </div>
-            <!-- プレビュー画像の表示 -->
-            <div id="preview" style="width: 300px;"></div>
-            <button type="submit">変更を保存する</button>
-        </form>
+    <div id="message"
+        class="hidden fixed top-[15%] left-1/2 transform -translate-x-1/2 text-white px-6 py-3 rounded-lg shadow-lg flex items-center space-x-4 z-50">
     </div>
-    <div class="footer">
-        <a
-            href="{{ route('work_story_posts.show', ['work_id' => $work_story_post->work_id, 'work_story_id' => $work_story_post->sub_title_id, 'work_story_post_id' => $work_story_post->id]) }}">保存しないで戻る</a>
+    <div class="container mx-auto px-4 py-6 grid grid-cols-1 lg:grid-cols-3 gap-6">
+        <div class="lg:col-span-2 space-y-6">
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                <div class="max-w-xl lg:max-w-2xl">
+                    <h2 class="text-lg font-medium text-gray-900">「{{ $work_story_post->workStory->sub_title }}」への投稿編集画面
+                    </h2>
+                    <p class="mt-1 text-sm text-gray-600">あらすじの感想を編集してみんなと共有しましょう！</p>
+                    <form
+                        action="{{ route('work_story_posts.update', ['work_id' => $work_story_post->work_id, 'work_story_id' => $work_story_post->sub_title_id, 'work_story_post_id' => $work_story_post->id]) }}"
+                        method="POST" enctype="multipart/form-data" class="mt-6 space-y-6">
+                        @csrf
+                        @method('PUT')
+                        <div class="work_id">
+                            <input type="hidden" name="work_story_post[work_id]"
+                                value="{{ $work_story_post->work_id }}">
+                        </div>
+                        <div class="user_id">
+                            <input type="hidden" name="work_story_post[user_id]"
+                                value="{{ $work_story_post->user_id }}">
+                        </div>
+                        <div class="sub_title_id">
+                            <input type="hidden" name="work_story_post[sub_title_id]"
+                                value="{{ $work_story_post->sub_title_id }}">
+                        </div>
+                        <div class="title">
+                            <label class="block font-medium text-sm text-gray-700 mb-2">タイトル</label>
+                            <input type="text" name="work_story_post[post_title]" placeholder="タイトル"
+                                value="{{ old('work_story_post.post_title', $work_story_post->post_title) }}"
+                                class="w-full border-gray-300 rounded-lg shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200"
+                                data-max-length="40" data-counter-id="titleCharacterCount"
+                                oninput="countCharacter(this)" />
+                            <p id="titleCharacterCount" class="mt-1 text-sm text-gray-500"></p>
+                            <p class="title__error text-sm text-red-500 mt-1">
+                                {{ $errors->first('work_story_post.post_title') }}</p>
+                        </div>
+                        <div class="star_num">
+                            <label class="block font-medium text-sm text-gray-700 mb-2">星の数</label>
+                            <select name="work_story_post[star_num]"
+                                class="block w-1/3 px-3 py-2 border border-gray-300 rounded-lg shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200">
+                                @php
+                                    $numbers = [1 => '★', 2 => '★★', 3 => '★★★', 4 => '★★★★', 5 => '★★★★★'];
+                                @endphp
+                                @foreach ($numbers as $num => $star)
+                                    <option value="{{ $num }}"
+                                        @if ($work_story_post->star_num == $num) selected @endif>
+                                        {{ $star }}
+                                    </option>
+                                @endforeach
+                            </select>
+                            @if ($errors->has('work_story_post.star_num'))
+                                <p class="mt-2 text-sm text-red-600">{{ $errors->first('work_story_post.star_num') }}
+                                </p>
+                            @endif
+                        </div>
+                        <div id="custom-multi-select-container" class="category relative">
+                            <label class="block font-medium text-sm text-gray-700 mb-2">カテゴリー（3個まで）</label>
+                            <div id="custom-multi-select" tabindex="0" class="w-1/3">
+                                <div id="custom-multi-select-list" class="max-h-48 overflow-y-auto">
+                                    @php
+                                        // old() が存在すればそれを使用し、なければ過去の保存値を使用
+                                        $existingCategories = $work_story_post->categories->pluck('id')->toArray();
+                                        $selectedCategories = old(
+                                            'work_story_post.categories_array',
+                                            $existingCategories,
+                                        );
+                                    @endphp
+                                    @foreach ($categories as $category)
+                                        <div class="custom-option p-2 cursor-pointer @if (in_array($category->id, $selectedCategories)) bg-gray-500 text-white @endif"
+                                            data-value="{{ $category->id }}" data-post-type="work_story_post">
+                                            {{ $category->name }}
+                                        </div>
+                                    @endforeach
+                                </div>
+                            </div>
+                            <!-- 選択された値を格納する -->
+                            <div id="selected-categories-container">
+                                @foreach ($selectedCategories as $selectedCategory)
+                                    <input type="hidden" name="work_story_post[categories_array][]"
+                                        value="{{ $selectedCategory }}">
+                                @endforeach
+                            </div>
+                            @if ($errors->has('work_story_post.categories_array'))
+                                <p class="category__error text-sm text-red-500 mt-1">
+                                    {{ $errors->first('work_story_post.categories_array') }}</p>
+                            @endif
+                        </div>
+                        <div class="body">
+                            <label class="block font-medium text-sm text-gray-700 mb-2">内容</label>
+                            <textarea name="work_story_post[body]" placeholder="内容を記入してください。"
+                                class="w-full border-gray-300 rounded-lg shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 h-40"
+                                data-max-length="4000" data-counter-id="bodyCharacterCount" oninput="countCharacter(this)">{{ old('work_story_post.body', $work_story_post->body) }}</textarea>
+                            <p id="bodyCharacterCount" class="mt-1 text-sm text-gray-500"></p>
+                            <p class="body__error text-sm text-red-500 mt-1">
+                                {{ $errors->first('work_story_post.body') }}
+                            </p>
+                        </div>
+                        <div class="image">
+                            <label class="block font-medium text-sm text-gray-700 mb-2">画像（4枚まで）</label>
+                            @php
+                                // 既にファイルが選択されている場合はそれらを表示する
+                                $existingImages = [];
+                                $numbers = [1, 2, 3, 4];
+                                foreach ($numbers as $number) {
+                                    $image = 'image' . $number;
+                                    if ($work_story_post->$image) {
+                                        array_push($existingImages, $work_story_post->$image);
+                                    }
+                                }
+                                $existingImages = json_encode($existingImages);
+                            @endphp
+                            <div id="existing_image_paths" data-php-variable="{{ $existingImages }}"></div>
+                            <label
+                                class="inline-flex items-center gap-2 cursor-pointer text-blue-500 bg-blue-20 border-2 border-gray-300 rounded-lg py-1 px-2 hover:bg-blue-50">
+                                <input id="inputElm" type="file" name="images[]" multiple class="hidden"
+                                    onchange="loadImage(this);"><span>画像を追加する</span>
+                            </label>
+                            <div id="count" class="text-sm text-gray-600 mt-1"></div>
+                            <!-- 画像トリミング用のモーダルウィンドウ表示 -->
+                            <div id="crop-modal"
+                                class="hidden fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+                                <div class="bg-white p-4 rounded-lg shadow-lg relative w-full max-w-2xl">
+                                    <div class="overflow-hidden w-full h-auto">
+                                        <img id="crop-preview" class="w-full h-auto object-cover" />
+                                    </div>
+                                    <div class="flex justify-end gap-2 mt-4">
+                                        <button id="crop-cancel-button" type="button"
+                                            class="px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-600">
+                                            キャンセル
+                                        </button>
+                                        <button id="crop-next-button" type="button"
+                                            class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+                                            次へ
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- プレビュー画像の表示 -->
+                            <div id="preview" class="grid grid-cols-2 gap-4 mt-4"></div>
+                            <!-- 削除された既存画像のリスト -->
+                            <input type="hidden" name="removedImages[]" id="removedImages" value="">
+                            <!-- 削除されず残った既存画像のリスト -->
+                            <input type="hidden" name="remainedImages[]" id="remainedImages" value="">
+                            <p class="image__error text-sm text-red-500 mt-1">{{ $errors->first('images') }}</p>
+                        </div>
+                        <!-- 投稿ボタン -->
+                        <div class="flex items-center  justify-center">
+                            <button type="submit"
+                                class="bg-blue-500 text-white py-2 px-4 rounded-lg shadow hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300">変更を保存する</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <!-- 右側サイドバーブロック -->
+        <div class="lg:col-span-1 bg-gray-100 rounded-lg shadow-md p-6">
+            <h2 class="text-lg font-bold">サイドバーコンテンツ</h2>
+            <ul class="space-y-2">
+                <li><a href="#" class="text-blue-500 hover:underline">リンク1</a></li>
+                <li><a href="#" class="text-blue-500 hover:underline">リンク2</a></li>
+                <li><a href="#" class="text-blue-500 hover:underline">リンク3</a></li>
+            </ul>
+        </div>
     </div>
     <script src="{{ asset('/js/edit_preview.js') }}"></script>
+    <script src="{{ asset('/js/select_multi_category.js') }}"></script>
+    <script src="{{ asset('/js/count_character.js') }}"></script>
+    <script>
+        // PHP の Helper 関数で定義した色データを JavaScript に渡す
+        const categoryColors = {!! json_encode([
+            'すべての画像のトリミングが完了しました' => getCategoryColor('すべての画像のトリミングが完了しました'),
+        ]) !!};
+    </script>
 </x-app-layout>
