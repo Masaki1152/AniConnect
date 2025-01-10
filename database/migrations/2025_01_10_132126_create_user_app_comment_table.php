5d4d8c012335f8c0039d8a393fdd1c7142bf2ae8
Masaki1152 <takanenoharusaku@gmail.com>
2025-01-10 13:33:06 +0900

    feat:聖地感想のコメントテーブルの作成

---
+++ database/migrations/2025_01_10_132126_create_user_app_comment_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_app_comment', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('app_comment_id')->constrained('pilgrimage_post_comments')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->timestamp('created_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_app_comment');
+    }
+};
