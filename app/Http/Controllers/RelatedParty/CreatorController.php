1fe4453cc576b5a81d8557d16ad1acf12455f424
Masaki1152 <takanenoharusaku@gmail.com>
2025-05-01 12:13:15 +0900

    refactor:RelatedPartyフォルダの追加

--- app/Http/Controllers/RelatedParty/CreatorController.php
+++ app/Http/Controllers/RelatedParty/CreatorController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\RelatedParty;
 
+use App\Http\Controllers\Controller;
 use App\Models\Creator;
 use Illuminate\Http\Request;
 
