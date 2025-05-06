781f1b8182113d6924f7f047589cc74febc55683
Masaki1152 <takanenoharusaku@gmail.com>
2025-05-06 11:46:10 +0900

    refactor:InputtextTypeを作品登録用に修正

--- app/Enums/InputTextType.php
+++ app/Enums/InputTextType.php
@@ -6,12 +6,24 @@ enum InputTextType: string
 {
     case Title = 'title';
     case Scene = 'scene';
+    case Name = 'name';
+    case Copyright = 'copyright';
+    case Term = 'term';
+    case OfficialSiteLink = 'official_site_link';
+    case WikiLink = 'wiki_link';
+    case TwitterLink = 'twitter_link';
 
     public function className(): string
     {
         return match ($this) {
             self::Title => 'title',
-            self::Scene => 'scene'
+            self::Scene => 'scene',
+            self::Name => 'name',
+            self::Copyright => 'copyright',
+            self::Term => 'term',
+            self::OfficialSiteLink => 'official_site_link',
+            self::WikiLink => 'wiki_link',
+            self::TwitterLink => 'twitter_link'
         };
     }
 
@@ -19,7 +31,13 @@ enum InputTextType: string
     {
         return match ($this) {
             self::Title => 'post_title',
-            self::Scene => 'scene'
+            self::Scene => 'scene',
+            self::Name => 'name',
+            self::Copyright => 'copyright',
+            self::Term => 'term',
+            self::OfficialSiteLink => 'official_site_link',
+            self::WikiLink => 'wiki_link',
+            self::TwitterLink => 'twitter_link'
         };
     }
 
@@ -27,7 +45,13 @@ enum InputTextType: string
     {
         return match ($this) {
             self::Title => 'common.title',
-            self::Scene => 'common.scene'
+            self::Scene => 'common.scene',
+            self::Name => 'common.name',
+            self::Copyright => 'common.copyright',
+            self::Term => 'common.term',
+            self::OfficialSiteLink => 'common.official_site_link',
+            self::WikiLink => 'common.wiki_link',
+            self::TwitterLink => 'common.twitter_link'
         };
     }
 }
