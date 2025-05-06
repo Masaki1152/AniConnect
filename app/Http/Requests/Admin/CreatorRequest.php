c9f73c8137b2911b1340207122205a594aae0efc
Masaki1152 <takanenoharusaku@gmail.com>
2025-05-06 22:00:30 +0900

    feature:管理者用制作会社の新規登録機能を追加

--- app/Http/Requests/Admin/CreatorRequest.php
+++ app/Http/Requests/Admin/CreatorRequest.php
@@ -1,28 +1,16 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Http\Requests\Admin;
 
 use Illuminate\Foundation\Http\FormRequest;
 
 class CreatorRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return false;
-    }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            //
+            'creators.name' => 'required|string|max:200',
+            'creators.wiki_link' => 'required|string|max:200'
         ];
     }
 }
