82ee718bd3cc45ec01e67b9a92ef59fbfb3124ae
Masaki1152 <takanenoharusaku@gmail.com>
2025-06-25 22:23:11 +0900

    feature:未ログインでも作品、あらすじ、音楽、登場人物、聖地や各感想一覧、詳細画面に遷移できるようにする

--- routes/web.php
+++ routes/web.php
@@ -195,13 +195,16 @@ Route::middleware('auth')->group(function () {
 });
 
 // WorkControllerに関するルーティング
-Route::controller(WorkController::class)->middleware(['auth'])->group(function () {
+Route::controller(WorkController::class)->group(function () {
     // 作品一覧の表示
     Route::get('/works', 'index')->name('works.index');
     // 各作品の詳細表示
     Route::get('/works/{work}', 'show')->name('works.show');
-    // 各作品の「気になる」ボタン押下で「気になる」登録をする処理
-    Route::post('/works/{work_id}/interested', 'interested')->name('works.interested');
+
+    Route::middleware(['auth'])->group(function () {
+        // 各作品の「気になる」ボタン押下で「気になる」登録をする処理
+        Route::post('/works/{work_id}/interested', 'interested')->name('works.interested');
+    });
 });
 
 // WorkInterestedControllerに関するルーティング
@@ -229,23 +232,26 @@ Route::controller(WrCommentLikeController::class)->middleware(['auth'])->group(f
 });
 
 // WorkReviewControllerに関するルーティング
-Route::controller(WorkReviewController::class)->middleware(['auth'])->group(function () {
+Route::controller(WorkReviewController::class)->group(function () {
     // 各作品ごとの感想投稿一覧の表示
     Route::get('/work_reviews/{work_id}', 'index')->name('work_reviews.index');
-    // 新規投稿作成ボタン押下で、createメソッドを実行
-    Route::get('/work_reviews/{work_id}/create', 'create')->name('work_reviews.create');
-    // 作成するボタン押下で、storeメソッドを実行
-    Route::post('/work_reviews/{work_id}/store', 'store')->name('work_reviews.store');
     // 各作品の感想投稿一覧ボタン押下で、showメソッドを実行
     Route::get('/work_reviews/{work_id}/reviews/{work_review_id}', 'show')->name('work_reviews.show');
-    // 感想投稿編集画面を表示するeditメソッドを実行
-    Route::get('/work_reviews/{work_id}/reviews/{work_review_id}/edit', 'edit')->name('work_reviews.edit');
-    // 感想投稿の編集を実行するupdateメソッドを実行
-    Route::put('/work_reviews/{work_id}/update/{work_review_id}', 'update')->name('work_reviews.update');
-    // 感想投稿の削除を行うdeleteメソッドを実行
-    Route::delete('/work_reviews/{work_id}/reviews/{work_review_id}/delete', 'delete')->name('work_reviews.delete');
-    // 感想投稿のいいねボタン押下で、いいねを追加するlikeメソッドを実行
-    Route::post('/work_reviews/{work_id}/reviews/{work_review_id}/like', 'like')->name('work_reviews.like');
+
+    Route::middleware(['auth'])->group(function () {
+        // 新規投稿作成ボタン押下で、createメソッドを実行
+        Route::get('/work_reviews/{work_id}/create', 'create')->name('work_reviews.create');
+        // 作成するボタン押下で、storeメソッドを実行
+        Route::post('/work_reviews/{work_id}/store', 'store')->name('work_reviews.store');
+        // 感想投稿編集画面を表示するeditメソッドを実行
+        Route::get('/work_reviews/{work_id}/reviews/{work_review_id}/edit', 'edit')->name('work_reviews.edit');
+        // 感想投稿の編集を実行するupdateメソッドを実行
+        Route::put('/work_reviews/{work_id}/update/{work_review_id}', 'update')->name('work_reviews.update');
+        // 感想投稿の削除を行うdeleteメソッドを実行
+        Route::delete('/work_reviews/{work_id}/reviews/{work_review_id}/delete', 'delete')->name('work_reviews.delete');
+        // 感想投稿のいいねボタン押下で、いいねを追加するlikeメソッドを実行
+        Route::post('/work_reviews/{work_id}/reviews/{work_review_id}/like', 'like')->name('work_reviews.like');
+    });
 });
 
 // WorkReviewLikeControllerに関するルーティング
@@ -263,13 +269,16 @@ Route::controller(CreatorController::class)->middleware(['auth'])->group(functio
 });
 
 // CharacterControllerに関するルーティング
-Route::controller(CharacterController::class)->middleware(['auth'])->group(function () {
+Route::controller(CharacterController::class)->group(function () {
     // 登場人物一覧の表示
     Route::get('/characters', 'index')->name('characters.index');
     // 登場人物の詳細表示
     Route::get('/characters/{character_id}', 'show')->name('characters.show');
-    // 各登場人物の「気になる」ボタン押下で「気になる」登録をする処理
-    Route::post('/characters/{character_id}/interested', 'interested')->name('characters.interested');
+
+    Route::middleware(['auth'])->group(function () {
+        // 各登場人物の「気になる」ボタン押下で「気になる」登録をする処理
+        Route::post('/characters/{character_id}/interested', 'interested')->name('characters.interested');
+    });
 });
 
 // CharacterInterestedControllerに関するルーティング
@@ -297,28 +306,31 @@ Route::controller(CpCommentLikeController::class)->middleware(['auth'])->group(f
 });
 
 // CharacterPostControllerに関するルーティング
-Route::controller(CharacterPostController::class)->middleware(['auth'])->group(function () {
+Route::controller(CharacterPostController::class)->group(function () {
     // 登場人物ごとの感想投稿一覧の表示
     Route::get('/character_posts/{character_id}', 'index')->name('character_posts.index');
-    // 新規投稿作成ボタン押下で、createメソッドを実行
-    Route::get('/character_posts/{character_id}/create', 'create')->name('character_posts.create');
-    // 作成するボタン押下で、storeメソッドを実行
-    Route::post('/character_posts/{character_id}/store', 'store')->name('character_posts.store');
     // 各登場人物の感想投稿一覧ボタン押下で、showメソッドを実行
     Route::get('/character_posts/{character_id}/posts/{character_post_id}', 'show')->name('character_posts.show');
-    // 感想投稿編集画面を表示するeditメソッドを実行
-    Route::get('/character_posts/{character_id}/posts/{character_post_id}/edit', 'edit')->name('character_posts.edit');
-    // 感想投稿の編集を実行するupdateメソッドを実行
-    Route::put('/character_posts/{character_id}/update/{character_post_id}', 'update')->name('character_posts.update');
-    // 感想投稿の削除を行うdeleteメソッドを実行
-    Route::delete('/character_posts/{character_id}/posts/{character_post_id}/delete', 'delete')->name('character_posts.delete');
-    // 感想投稿のいいねボタン押下で、いいねを追加するlikeメソッドを実行
-    Route::post('/character_posts/{character_id}/posts/{character_post_id}/like', 'like')->name('character_posts.like');
+
+    Route::middleware(['auth'])->group(function () {
+        // 新規投稿作成ボタン押下で、createメソッドを実行
+        Route::get('/character_posts/{character_id}/create', 'create')->name('character_posts.create');
+        // 作成するボタン押下で、storeメソッドを実行
+        Route::post('/character_posts/{character_id}/store', 'store')->name('character_posts.store');
+        // 感想投稿編集画面を表示するeditメソッドを実行
+        Route::get('/character_posts/{character_id}/posts/{character_post_id}/edit', 'edit')->name('character_posts.edit');
+        // 感想投稿の編集を実行するupdateメソッドを実行
+        Route::put('/character_posts/{character_id}/update/{character_post_id}', 'update')->name('character_posts.update');
+        // 感想投稿の削除を行うdeleteメソッドを実行
+        Route::delete('/character_posts/{character_id}/posts/{character_post_id}/delete', 'delete')->name('character_posts.delete');
+        // 感想投稿のいいねボタン押下で、いいねを追加するlikeメソッドを実行
+        Route::post('/character_posts/{character_id}/posts/{character_post_id}/like', 'like')->name('character_posts.like');
+    });
 });
 
 // CharacterPostLikeControllerに関するルーティング
 Route::controller(CharacterPostLikeController::class)->middleware(['auth'])->group(function () {
-    // 作品一覧の表示
+    // いいねしたユーザーの表示
     Route::get('/character_posts/{character_id}/posts/{character_post_id}/like/index', 'index')->name('character_post_like.index');
 });
 
@@ -329,13 +341,16 @@ Route::controller(VoiceArtistController::class)->middleware(['auth'])->group(fun
 });
 
 // MusicControllerに関するルーティング
-Route::controller(MusicController::class)->middleware(['auth'])->group(function () {
+Route::controller(MusicController::class)->group(function () {
     // 音楽一覧の表示
     Route::get('/music', 'index')->name('music.index');
     // 音楽の詳細表示
     Route::get('/music/{music_id}', 'show')->name('music.show');
-    // 音楽の「気になる」ボタン押下で「気になる」登録をする処理
-    Route::post('/music/{music_id}/interested', 'interested')->name('music.interested');
+
+    Route::middleware(['auth'])->group(function () {
+        // 音楽の「気になる」ボタン押下で「気になる」登録をする処理
+        Route::post('/music/{music_id}/interested', 'interested')->name('music.interested');
+    });
 });
 
 // MusicInterestedControllerに関するルーティング
@@ -363,28 +378,31 @@ Route::controller(MpCommentLikeController::class)->middleware(['auth'])->group(f
 });
 
 // MusicPostControllerに関するルーティング
-Route::controller(MusicPostController::class)->middleware(['auth'])->group(function () {
+Route::controller(MusicPostController::class)->group(function () {
     // 音楽人物ごとの感想投稿一覧の表示
     Route::get('/music_posts/{music_id}', 'index')->name('music_posts.index');
-    // 新規投稿作成ボタン押下で、createメソッドを実行
-    Route::get('/music_posts/{music_id}/create', 'create')->name('music_posts.create');
-    // 作成するボタン押下で、storeメソッドを実行
-    Route::post('/music_posts/{music_id}/store', 'store')->name('music_posts.store');
     // 各登場人物の感想投稿一覧ボタン押下で、showメソッドを実行
     Route::get('/music_posts/{music_id}/posts/{music_post_id}', 'show')->name('music_posts.show');
-    // 感想投稿編集画面を表示するeditメソッドを実行
-    Route::get('/music_posts/{music_id}/posts/{music_post_id}/edit', 'edit')->name('music_posts.edit');
-    // 感想投稿の編集を実行するupdateメソッドを実行
-    Route::put('/music_posts/{music_id}/update/{music_post_id}', 'update')->name('music_posts.update');
-    // 感想投稿の削除を行うdeleteメソッドを実行
-    Route::delete('/music_posts/{music_id}/posts/{music_post_id}/delete', 'delete')->name('music_posts.delete');
-    // 感想投稿のいいねボタン押下で、いいねを追加するlikeメソッドを実行
-    Route::post('/music_posts/{music_id}/posts/{music_post_id}/like', 'like')->name('music_posts.like');
+
+    Route::middleware(['auth'])->group(function () {
+        // 新規投稿作成ボタン押下で、createメソッドを実行
+        Route::get('/music_posts/{music_id}/create', 'create')->name('music_posts.create');
+        // 作成するボタン押下で、storeメソッドを実行
+        Route::post('/music_posts/{music_id}/store', 'store')->name('music_posts.store');
+        // 感想投稿編集画面を表示するeditメソッドを実行
+        Route::get('/music_posts/{music_id}/posts/{music_post_id}/edit', 'edit')->name('music_posts.edit');
+        // 感想投稿の編集を実行するupdateメソッドを実行
+        Route::put('/music_posts/{music_id}/update/{music_post_id}', 'update')->name('music_posts.update');
+        // 感想投稿の削除を行うdeleteメソッドを実行
+        Route::delete('/music_posts/{music_id}/posts/{music_post_id}/delete', 'delete')->name('music_posts.delete');
+        // 感想投稿のいいねボタン押下で、いいねを追加するlikeメソッドを実行
+        Route::post('/music_posts/{music_id}/posts/{music_post_id}/like', 'like')->name('music_posts.like');
+    });
 });
 
 // MusicPostLikeControllerに関するルーティング
 Route::controller(MusicPostLikeController::class)->middleware(['auth'])->group(function () {
-    // 作品一覧の表示
+    // いいねしたユーザーの表示
     Route::get('/music_posts/{music_id}/posts/{music_post_id}/like/index', 'index')->name('music_post_like.index');
 });
 
@@ -407,13 +425,16 @@ Route::controller(ComposerController::class)->middleware(['auth'])->group(functi
 });
 
 // AnimePilgrimageControllerに関するルーティング
-Route::controller(AnimePilgrimageController::class)->middleware(['auth'])->group(function () {
+Route::controller(AnimePilgrimageController::class)->group(function () {
     // 聖地一覧の表示
     Route::get('/pilgrimages', 'index')->name('pilgrimages.index');
     // 聖地の詳細表示
     Route::get('/pilgrimages/{pilgrimage_id}', 'show')->name('pilgrimages.show');
-    // 聖地の「気になる」ボタン押下で「気になる」登録をする処理
-    Route::post('/pilgrimages/{pilgrimage_id}/interested', 'interested')->name('pilgrimages.interested');
+
+    Route::middleware(['auth'])->group(function () {
+        // 聖地の「気になる」ボタン押下で「気になる」登録をする処理
+        Route::post('/pilgrimages/{pilgrimage_id}/interested', 'interested')->name('pilgrimages.interested');
+    });
 });
 
 // AnimePilgrimageInterestedControllerに関するルーティング
@@ -441,39 +462,45 @@ Route::controller(AppCommentLikeController::class)->middleware(['auth'])->group(
 });
 
 // AnimePilgrimagePostControllerに関するルーティング
-Route::controller(AnimePilgrimagePostController::class)->middleware(['auth'])->group(function () {
+Route::controller(AnimePilgrimagePostController::class)->group(function () {
     // 聖地ごとの感想投稿一覧の表示
     Route::get('/pilgrimage_posts/{pilgrimage_id}', 'index')->name('pilgrimage_posts.index');
-    // 新規投稿作成ボタン押下で、createメソッドを実行
-    Route::get('/pilgrimage_posts/{pilgrimage_id}/create', 'create')->name('pilgrimage_posts.create');
-    // 作成するボタン押下で、storeメソッドを実行
-    Route::post('/pilgrimage_posts/{pilgrimage_id}/store', 'store')->name('pilgrimage_posts.store');
     // 各聖地の感想投稿一覧ボタン押下で、showメソッドを実行
     Route::get('/pilgrimage_posts/{pilgrimage_id}/posts/{pilgrimage_post_id}', 'show')->name('pilgrimage_posts.show');
-    // 感想投稿編集画面を表示するeditメソッドを実行
-    Route::get('/pilgrimage_posts/{pilgrimage_id}/posts/{pilgrimage_post_id}/edit', 'edit')->name('pilgrimage_posts.edit');
-    // 感想投稿の編集を実行するupdateメソッドを実行
-    Route::put('/pilgrimage_posts/{pilgrimage_id}/update/{pilgrimage_post_id}', 'update')->name('pilgrimage_posts.update');
-    // 感想投稿の削除を行うdeleteメソッドを実行
-    Route::delete('/pilgrimage_posts/{pilgrimage_id}/posts/{pilgrimage_post_id}/delete', 'delete')->name('pilgrimage_posts.delete');
-    // 感想投稿のいいねボタン押下で、いいねを追加するlikeメソッドを実行
-    Route::post('/pilgrimage_posts/{pilgrimage_id}/posts/{pilgrimage_post_id}/like', 'like')->name('pilgrimage_posts.like');
+
+    Route::middleware(['auth'])->group(function () {
+        // 新規投稿作成ボタン押下で、createメソッドを実行
+        Route::get('/pilgrimage_posts/{pilgrimage_id}/create', 'create')->name('pilgrimage_posts.create');
+        // 作成するボタン押下で、storeメソッドを実行
+        Route::post('/pilgrimage_posts/{pilgrimage_id}/store', 'store')->name('pilgrimage_posts.store');
+        // 感想投稿編集画面を表示するeditメソッドを実行
+        Route::get('/pilgrimage_posts/{pilgrimage_id}/posts/{pilgrimage_post_id}/edit', 'edit')->name('pilgrimage_posts.edit');
+        // 感想投稿の編集を実行するupdateメソッドを実行
+        Route::put('/pilgrimage_posts/{pilgrimage_id}/update/{pilgrimage_post_id}', 'update')->name('pilgrimage_posts.update');
+        // 感想投稿の削除を行うdeleteメソッドを実行
+        Route::delete('/pilgrimage_posts/{pilgrimage_id}/posts/{pilgrimage_post_id}/delete', 'delete')->name('pilgrimage_posts.delete');
+        // 感想投稿のいいねボタン押下で、いいねを追加するlikeメソッドを実行
+        Route::post('/pilgrimage_posts/{pilgrimage_id}/posts/{pilgrimage_post_id}/like', 'like')->name('pilgrimage_posts.like');
+    });
 });
 
 // AnimePilgrimagePostLikeControllerに関するルーティング
 Route::controller(AnimePilgrimagePostLikeController::class)->middleware(['auth'])->group(function () {
-    // 作品一覧の表示
+    // いいねしたユーザーの表示
     Route::get('/pilgrimage_posts/{pilgrimage_id}/posts/{pilgrimage_post_id}/like/index', 'index')->name('pilgrimage_post_like.index');
 });
 
 // WorkStoryControllerに関するルーティング
-Route::controller(WorkStoryController::class)->middleware(['auth'])->group(function () {
+Route::controller(WorkStoryController::class)->group(function () {
     // あらすじ一覧の表示
     Route::get('/works/{work_id}/stories', 'index')->name('work_stories.index');
     // あらすじの詳細表示
     Route::get('/works/{work_id}/stories/{work_story_id}', 'show')->name('work_stories.show');
-    // 各あらすじの「気になる」ボタン押下で「気になる」登録をする処理
-    Route::post('/works/{work_id}/stories/{work_story_id}/interested', 'interested')->name('work_stories.interested');
+
+    Route::middleware(['auth'])->group(function () {
+        // 各あらすじの「気になる」ボタン押下で「気になる」登録をする処理
+        Route::post('/works/{work_id}/stories/{work_story_id}/interested', 'interested')->name('work_stories.interested');
+    });
 });
 
 // WorkStoryInterestedControllerに関するルーティング
@@ -501,23 +528,26 @@ Route::controller(WspCommentLikeController::class)->middleware(['auth'])->group(
 });
 
 // WorkStoryPostControllerに関するルーティング
-Route::controller(WorkStoryPostController::class)->middleware(['auth'])->group(function () {
+Route::controller(WorkStoryPostController::class)->group(function () {
     // あらすじごとの感想投稿一覧の表示
     Route::get('/works/{work_id}/stories/{work_story_id}/posts', 'index')->name('work_story_posts.index');
-    // 新規投稿作成ボタン押下で、createメソッドを実行
-    Route::get('/works/{work_id}/stories/{work_story_id}/create', 'create')->name('work_story_posts.create');
-    // 作成するボタン押下で、storeメソッドを実行
-    Route::post('/works/{work_id}/stories/{work_story_id}/posts/store', 'store')->name('work_story_posts.store');
     // 各あらすじの感想投稿一覧ボタン押下で、showメソッドを実行
     Route::get('/works/{work_id}/stories/{work_story_id}/posts/{work_story_post_id}', 'show')->name('work_story_posts.show');
-    // 感想投稿編集画面を表示するeditメソッドを実行
-    Route::get('/works/{work_id}/stories/{work_story_id}/posts/{work_story_post_id}/edit', 'edit')->name('work_story_posts.edit');
-    // 感想投稿の編集を実行するupdateメソッドを実行
-    Route::put('/works/{work_id}/stories/{work_story_id}/posts/{work_story_post_id}/update', 'update')->name('work_story_posts.update');
-    // 感想投稿の削除を行うdeleteメソッドを実行
-    Route::delete('/works/{work_id}/stories/{work_story_id}/posts/{work_story_post_id}/delete', 'delete')->name('work_story_posts.delete');
-    // 感想投稿のいいねボタン押下で、いいねを追加するlikeメソッドを実行
-    Route::post('/works/{work_id}/stories/{work_story_id}/posts/{work_story_post_id}/like', 'like')->name('work_story_posts.like');
+
+    Route::middleware(['auth'])->group(function () {
+        // 新規投稿作成ボタン押下で、createメソッドを実行
+        Route::get('/works/{work_id}/stories/{work_story_id}/create', 'create')->name('work_story_posts.create');
+        // 作成するボタン押下で、storeメソッドを実行
+        Route::post('/works/{work_id}/stories/{work_story_id}/posts/store', 'store')->name('work_story_posts.store');
+        // 感想投稿編集画面を表示するeditメソッドを実行
+        Route::get('/works/{work_id}/stories/{work_story_id}/posts/{work_story_post_id}/edit', 'edit')->name('work_story_posts.edit');
+        // 感想投稿の編集を実行するupdateメソッドを実行
+        Route::put('/works/{work_id}/stories/{work_story_id}/posts/{work_story_post_id}/update', 'update')->name('work_story_posts.update');
+        // 感想投稿の削除を行うdeleteメソッドを実行
+        Route::delete('/works/{work_id}/stories/{work_story_id}/posts/{work_story_post_id}/delete', 'delete')->name('work_story_posts.delete');
+        // 感想投稿のいいねボタン押下で、いいねを追加するlikeメソッドを実行
+        Route::post('/works/{work_id}/stories/{work_story_id}/posts/{work_story_post_id}/like', 'like')->name('work_story_posts.like');
+    });
 });
 
 // WorkStoryPostLikeControllerに関するルーティング
