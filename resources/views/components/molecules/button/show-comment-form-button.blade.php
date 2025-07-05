724d7ea2b4f71567ccf4a4e10908cbc6ad9b7b61
Masaki1152 <takanenoharusaku@gmail.com>
2025-07-05 23:36:44 +0900

    feature:ShowCommentFormButtonをコメント内のボタンにも使えるように改修

--- resources/views/components/molecules/button/show-comment-form-button.blade.php
+++ resources/views/components/molecules/button/show-comment-form-button.blade.php
@@ -1,8 +1,17 @@
-<div class='content_fotter_comment'>
-    <button id='toggleComments' type='button'
+@if ($comment)
+    <button id='toggleChildComments-{{ $comment->id }}' type='button'
         class="px-2 py-1 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600"
-        onclick="toggleCommentForm()">{{ __('common.comment') }}</button>
-    <button id='closeComments' type='button'
+        onclick="toggleChildCommentForm({{ $comment->id }})">{{ __('common.comment') }}</button>
+    <button id='closeChildComments-{{ $comment->id }}' type='button'
         class="px-2 py-1 bg-gray-300 text-gray-700 rounded-lg shadow-md hover:bg-gray-400 hidden"
-        onclick="toggleCommentForm()">{{ __('common.close') }}</button>
-</div>
+        onclick="toggleChildCommentForm({{ $comment->id }})">{{ __('common.close') }}</button>
+@else
+    <div class='content_fotter_comment'>
+        <button id='toggleComments' type='button'
+            class="px-2 py-1 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600"
+            onclick="toggleCommentForm()">{{ __('common.comment') }}</button>
+        <button id='closeComments' type='button'
+            class="px-2 py-1 bg-gray-300 text-gray-700 rounded-lg shadow-md hover:bg-gray-400 hidden"
+            onclick="toggleCommentForm()">{{ __('common.close') }}</button>
+    </div>
+@endif
