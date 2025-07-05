e6949eb5f37f231b312d7ed6407371fa26861794
Masaki1152 <takanenoharusaku@gmail.com>
2025-07-05 23:17:51 +0900

    feature:コメント内のいいねボタンのコンポーネントを作成

---
+++ resources/views/components/molecules/button/like-comment-button.blade.php
@@ -0,0 +1,16 @@
+<div class='comment-like flex items-center gap-2'>
+    <!-- ボタンの見た目は後のデザイン作成の際に設定する予定 -->
+    <button id="comment-like_button-{{ $comment->id }}" data-comment-id="{{ $comment->id }}"
+        onclick="toggleLike({{ $comment->id }}, 'comment-like_button-{{ $comment->id }}', 'comment-like_count-{{ $comment->id }}', '{{ $baseRoute }}s')"
+        class="comment-like_button px-2 py-1 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600">
+        {{ $comment->users->contains(auth()->user()) ? __('common.unlike_action') : __('common.like_action') }}
+    </button>
+    <div class="comment-like_user">
+        <a href="{{ route($baseRoute . '_comment.like.index', ['comment_id' => $comment->id]) }}"
+            class="text-lg font-medium text-gray-700">
+            <p id="comment-like_count-{{ $comment->id }}">
+                {{ $comment->users->count() }}件
+            </p>
+        </a>
+    </div>
+</div>
