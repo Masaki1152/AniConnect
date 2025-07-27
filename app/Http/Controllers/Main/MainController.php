2e08e4092066400cd7a5a468c4b43830a11b297f
Masaki1152 <takanenoharusaku@gmail.com>
2025-07-28 01:31:34 +0900

    feature:MainControllerにてあらすじ、音楽、聖地の上位3つを取得するように処理を追加

--- app/Http/Controllers/Main/MainController.php
+++ app/Http/Controllers/Main/MainController.php
@@ -5,7 +5,10 @@ namespace App\Http\Controllers\Main;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Work;
+use App\Models\WorkStory;
 use App\Models\Character;
+use App\Models\Music;
+use App\Models\AnimePilgrimage;
 use App\Models\Notification;
 use Illuminate\Support\Facades\Cache;
 
@@ -13,7 +16,7 @@ use Illuminate\Support\Facades\Cache;
 class MainController extends Controller
 {
     // メイン画面の表示
-    public function index(Work $work, Character $character, Notification $notification)
+    public function index(Work $work, WorkStory $workStory, Character $character, Music $music, AnimePilgrimage $animePilgrimage, Notification $notification)
     {
         // 人気上位の作品を取得
         $topPopularityWorks = Cache::get('top_popular_works');
@@ -26,6 +29,14 @@ class MainController extends Controller
             $topPopularityWorks = Cache::get('top_popular_works');
         }
 
+        // 人気上位のあらすじを取得(毎度作品ごとに異なるためforgetを行う)
+        Cache::forget('top_popular_work_stories');
+        $sufficientPostsWorkStories = $workStory->fetchSufficientPostNumWorkStories();
+        // updateTopPopularityItemsを実行して人気度の高い作品を再計算
+        $workStory->updateTopPopularityItems($sufficientPostsWorkStories, 'workStoryPosts', 'top_popular_work_stories');
+        // キャッシュから人気度の高い作品を取得
+        $topPopularityWorkStories = Cache::get('top_popular_work_stories');
+
         // 人気上位の登場人物を取得
         $topPopularityCharacters = Cache::get('top_popular_characters');
         // キャッシュが見つからない場合
@@ -37,8 +48,37 @@ class MainController extends Controller
             $topPopularityCharacters = Cache::get('top_popular_characters');
         }
 
+        // 人気上位の音楽を取得
+        $topPopularityMusic = Cache::get('top_popular_music');
+        // キャッシュが見つからない場合
+        if (!$topPopularityMusic) {
+            $sufficientPostsMusic = $music->fetchSufficientPostNumMusic();
+            // updateTopPopularityItemsを実行して人気度の高い音楽を再計算
+            $music->updateTopPopularityItems($sufficientPostsMusic, 'musicPosts', 'top_popular_music');
+            // 再度キャッシュから人気度の高い音楽を取得
+            $topPopularityMusic = Cache::get('top_popular_music');
+        }
+
+        // 人気上位の聖地を取得
+        $topPopularityPilgrimages = Cache::get('top_popular_pilgrimages');
+        // キャッシュが見つからない場合
+        if (!$topPopularityPilgrimages) {
+            $sufficientPostsPilgrimages = $animePilgrimage->fetchSufficientPostNumPilgrimages();
+            // updateTopPopularityItemsを実行して人気度の高い作品を再計算
+            $animePilgrimage->updateTopPopularityItems($sufficientPostsPilgrimages, 'animePilgrimagePosts', 'top_popular_pilgrimages');
+            // 再度キャッシュから人気度の高い作品を取得
+            $topPopularityPilgrimages = Cache::get('top_popular_pilgrimages');
+        }
+
         // 最新のお知らせ4件の取得
         $notifications = $notification->getRecentNotifications();
-        return view('main.index', compact('notifications', 'topPopularityWorks', 'topPopularityCharacters'));
+        return view('main.index', compact(
+            'notifications',
+            'topPopularityWorks',
+            'topPopularityWorkStories',
+            'topPopularityCharacters',
+            'topPopularityMusic',
+            'topPopularityPilgrimages'
+        ));
     }
 }
