389eb7515bfcbd8cbf46c640ab567082dd6abcad
さとこっち <114640142+Masaki1152@users.noreply.github.com>
2025-07-31 03:27:48 +0900

    Update PopularThreeItemsSection.php

--- app/View/Components/Organisms/PopularThreeItemsSection.php
+++ app/View/Components/Organisms/PopularThreeItemsSection.php
@@ -3,6 +3,7 @@
 namespace App\View\Components\Organisms;
 
 use Illuminate\View\Component;
+use Illuminate\Support\Facades\Log;
 
 class PopularThreeItemsSection extends Component
 {
@@ -13,7 +14,18 @@ class PopularThreeItemsSection extends Component
     public function __construct($popularItems)
     {
         $this->popularItems = $popularItems;
-        $this->itemType = $popularItems[0]['itemType'];
+        //$this->itemType = $popularItems[0]['itemType'];
+
+        Log::info('PopularThreeItemsSection: Constructor called.');
+        Log::info('PopularThreeItemsSection: $popularItems received:', ['data' => $popularItems]);
+
+        if (!empty($popularItems) && isset($popularItems[0]['itemType'])) {
+            $this->itemType = $popularItems[0]['itemType'];
+            Log::info('PopularThreeItemsSection: itemType successfully extracted: ' . $this->itemType);
+        } else {
+            $this->itemType = null;
+            Log::warning('PopularThreeItemsSection: $popularItems is empty or "itemType" is missing for the first item. Setting itemType to null.', ['received_popularItems' => $popularItems]);
+        }
     }
 
     public function render()
