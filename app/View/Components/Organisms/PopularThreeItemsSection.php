c2f159ee2b8faa359dafac6a35a09f23236bfb5c
さとこっち <114640142+Masaki1152@users.noreply.github.com>
2025-07-31 02:43:44 +0900

    Update PopularThreeItemsSection.php

--- app/View/Components/Organisms/PopularThreeItemsSection.php
+++ app/View/Components/Organisms/PopularThreeItemsSection.php
@@ -3,7 +3,6 @@
 namespace App\View\Components\Organisms;
 
 use Illuminate\View\Component;
-use Illuminate\Support\Facades\Log;
 
 class PopularThreeItemsSection extends Component
 {
@@ -14,14 +13,7 @@ class PopularThreeItemsSection extends Component
     public function __construct($popularItems)
     {
         $this->popularItems = $popularItems;
-        //$this->itemType = $popularItems[0]['itemType'];
-        if (!empty($popularItems) && isset($popularItems[0]['itemType'])) {
-            $this->itemType = $popularItems[0]['itemType'];
-            Log::info('PopularThreeItemsSection: Data received successfully.', ['itemType' => $this->itemType, 'popularItemsCount' => count($popularItems)]);
-        } else {
-            $this->itemType = null;
-            Log::warning('PopularThreeItemsSection: $popularItems is empty or "itemType" is missing for the first item.', ['popularItems' => $popularItems]);
-        }
+        $this->itemType = $popularItems[0]['itemType'];
     }
 
     public function render()
