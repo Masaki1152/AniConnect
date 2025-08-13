6baea229ad75fdb05aee672fa39cf9d0926d3e9a
Masaki1152 <takanenoharusaku@gmail.com>
2025-08-13 14:00:58 +0900

    feature:AP-01-02 プライバシーポリシーのweb用レイアウトの作成

---
+++ resources/views/main/privacy_policy/show.blade.php
@@ -0,0 +1,170 @@
+<x-app-layout>
+    <div class="py-10">
+        <div class="w-full max-w-[888px] mx-auto">
+            <div class="privacy-policy-content px-9 flex flex-col gap-8">
+                <div class="privacy-policy-title">
+                    <div class="text-2xl font-bold text-textColor border-b-4 border-mainColor pb-2 w-fit">
+                        {{ __('common.privacy_policy') }}
+                    </div>
+                    <p class="mt-5 ml-10 text-base text-textColor text-justify">
+                        {{ __('entity.privacy_policy.description') }}
+                    </p>
+                </div>
+                <div class="privacy-policy-1 mt-2 mx-6">
+                    <div class="text-xl font-semibold text-textColor border-b-2 border-mainColor pb-2 w-fit">
+                        {{ __('entity.privacy_policy.title_01') }}
+                    </div>
+                    <p class="mt-3 ml-10 text-base text-textColor text-justify">
+                        {{ __('entity.privacy_policy.description_01_01') }}
+                    </p>
+                </div>
+                <div class="privacy-policy-2 mx-6">
+                    <div class="text-xl font-semibold text-textColor border-b-2 border-mainColor pb-2 w-fit">
+                        {{ __('entity.privacy_policy.title_02') }}
+                    </div>
+                    <p class="mt-3 ml-10 text-base text-textColor text-justify">
+                        {{ __('entity.privacy_policy.description_02_01') }}
+                    </p>
+                </div>
+                <div class="privacy-policy-3 mx-6">
+                    <div class="text-xl font-semibold text-textColor border-b-2 border-mainColor pb-2 w-fit">
+                        {{ __('entity.privacy_policy.title_03') }}
+                    </div>
+                    <p class="mt-3 ml-10 text-base text-textColor text-justify">
+                        {{ __('entity.privacy_policy.description_03_01') }}
+                    </p>
+                    <div class="description-list pl-20 mt-2">
+                        <ul class="list-disc list-outside text-base text-textColor text-justify space-y-1">
+                            <li>{{ __('entity.privacy_policy.description_03_02') }}</li>
+                            <li>{{ __('entity.privacy_policy.description_03_03') }}</li>
+                            <li>{{ __('entity.privacy_policy.description_03_04') }}</li>
+                            <li>{{ __('entity.privacy_policy.description_03_05') }}</li>
+                            <li>{{ __('entity.privacy_policy.description_03_06') }}</li>
+                            <li>{{ __('entity.privacy_policy.description_03_07') }}</li>
+                            <li>{{ __('entity.privacy_policy.description_03_08') }}</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="privacy-policy-4 mx-6">
+                    <div class="text-xl font-semibold text-textColor border-b-2 border-mainColor pb-2 w-fit">
+                        {{ __('entity.privacy_policy.title_04') }}
+                    </div>
+                    <div class="description-list pl-14 mt-3">
+                        <ul class="list-disc list-outside text-base text-textColor text-justify space-y-1">
+                            <li>{{ __('entity.privacy_policy.description_04_01') }}</li>
+                            <li>{{ __('entity.privacy_policy.description_04_02') }}</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="privacy-policy-5 mx-6">
+                    <div class="text-xl font-semibold text-textColor border-b-2 border-mainColor pb-2 w-fit">
+                        {{ __('entity.privacy_policy.title_05') }}
+                    </div>
+                    <div class="description-list pl-14 mt-3">
+                        <ul class="list-disc list-outside text-base text-textColor text-justify space-y-3">
+                            <li>
+                                {{ __('entity.privacy_policy.description_05_01') }}
+                                <ul class="list-circle list-outside mt-3 ml-6 space-y-1">
+                                    <li>{{ __('entity.privacy_policy.description_05_02') }}</li>
+                                    <li>{{ __('entity.privacy_policy.description_05_03') }}</li>
+                                    <li>{{ __('entity.privacy_policy.description_05_04') }}</li>
+                                    <li>
+                                        {{ __('entity.privacy_policy.description_05_05') }}
+                                        <ul class="list-square list-outside mt-3 ml-6 space-y-1">
+                                            <li>{{ __('entity.privacy_policy.description_05_06') }}</li>
+                                            <li>{{ __('entity.privacy_policy.description_05_07') }}</li>
+                                            <li>{{ __('entity.privacy_policy.description_05_07') }}</li>
+                                            <li>{{ __('entity.privacy_policy.description_05_09') }}</li>
+                                            <li>{{ __('entity.privacy_policy.description_05_10') }}</li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li>
+                                {{ __('entity.privacy_policy.description_05_11') }}
+                                <ul class="list-circle list-outside mt-3 ml-6 space-y-1">
+                                    <li>{{ __('entity.privacy_policy.description_05_12') }}</li>
+                                    <li>{{ __('entity.privacy_policy.description_05_13') }}</li>
+                                    <li>{{ __('entity.privacy_policy.description_05_14') }}</li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="privacy-policy-6 mx-6">
+                    <div class="text-xl font-semibold text-textColor border-b-2 border-mainColor pb-2 w-fit">
+                        {{ __('entity.privacy_policy.title_06') }}
+                    </div>
+                    <div class="description-list pl-14 mt-3">
+                        <ul class="list-disc list-outside text-base text-textColor text-justify space-y-3">
+                            <li>
+                                {{ __('entity.privacy_policy.description_06_01') }}
+                                <ul class="list-circle list-outside mt-3 ml-6 space-y-1">
+                                    <li>{{ __('entity.privacy_policy.description_06_02') }}</li>
+                                    <li>{{ __('entity.privacy_policy.description_06_03') }}</li>
+                                    <li>{{ __('entity.privacy_policy.description_06_04') }}</li>
+                                </ul>
+                            </li>
+                            <li>
+                                {{ __('entity.privacy_policy.description_06_05') }}
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="privacy-policy-7 mx-6">
+                    <div class="text-xl font-semibold text-textColor border-b-2 border-mainColor pb-2 w-fit">
+                        {{ __('entity.privacy_policy.title_07') }}
+                    </div>
+                    <div class="description-list pl-14 mt-3">
+                        <ul class="list-disc list-outside text-base text-textColor text-justify space-y-3">
+                            <li>{{ __('entity.privacy_policy.description_07_01') }}</li>
+                            <li>{{ __('entity.privacy_policy.description_07_02') }}</li>
+                            <li>{{ __('entity.privacy_policy.description_07_03') }}</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="privacy-policy-8 mx-6">
+                    <div class="text-xl font-semibold text-textColor border-b-2 border-mainColor pb-2 w-fit">
+                        {{ __('entity.privacy_policy.title_08') }}
+                    </div>
+                    <div class="description-list pl-14 mt-3">
+                        <ul class="list-disc list-outside text-base text-textColor text-justify space-y-3">
+                            <li>{{ __('entity.privacy_policy.description_08_01') }}</li>
+                            <li>{{ __('entity.privacy_policy.description_08_02') }}</li>
+                            <li>{{ __('entity.privacy_policy.description_08_03') }}</li>
+                            <li>{{ __('entity.privacy_policy.description_08_04') }}</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="privacy-policy-9 mx-6">
+                    <div class="text-xl font-semibold text-textColor border-b-2 border-mainColor pb-2 w-fit">
+                        {{ __('entity.privacy_policy.title_09') }}
+                    </div>
+                    <div class="description-list pl-14 mt-3">
+                        <ul class="list-disc list-outside text-base text-textColor text-justify space-y-3">
+                            <li>{{ __('entity.privacy_policy.description_09_01') }}</li>
+                            <li>{{ __('entity.privacy_policy.description_09_02') }}</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="privacy-policy-10 mx-6">
+                    <div class="text-xl font-semibold text-textColor border-b-2 border-mainColor pb-2 w-fit">
+                        {{ __('entity.privacy_policy.title_10') }}
+                    </div>
+                    <div class="mt-3 ml-10 text-base text-textColor">
+                        {{ __('entity.privacy_policy.description_10_01') }}
+                        <p class="mt-3 ml-10">
+                            {{ __('entity.privacy_policy.description_10_02') }}
+                            <a href="#" class="text-linkColor hover:underline">{{ __('common.here') }}</a>
+                        </p>
+                    </div>
+                </div>
+                <div class="privacy-policy-end mt-2 mx-6">
+                    <div class="flex justify-end">
+                        {{ __('entity.end') }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
