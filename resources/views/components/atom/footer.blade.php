f43c8ae16294460f331a34f3660dca7fdb9d8832
Masaki1152 <takanenoharusaku@gmail.com>
2025-08-13 14:00:06 +0900

    feature:プライバシーポリシー画面を表示するための準備

--- resources/views/components/atom/footer.blade.php
+++ resources/views/components/atom/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex flex-col lg:flex-row justify-between gap-4">
             <div class="flex flex-col lg:flex-row gap-2 lg:gap-8 ml-0 lg:ml-8">
-                <a href="/privacy-policy" class="hover:underline">{{ __('common.privacy_policy') }}</a>
+                <a href="{{ route('privacy_policy.show') }}" class="hover:underline">{{ __('common.privacy_policy') }}</a>
                 <a href="/terms" class="hover:underline">{{ __('common.terms') }}</a>
                 <a href="/contact" class="hover:underline">{{ __('common.contact') }}</a>
             </div>
